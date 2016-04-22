fbd647420dd96280112d385c4f45b464de162f5c
roku6185 <robin@alike.se>
2016-04-22 21:12:13 +0200

    Added function to read from socket

---
+++ roku_redis_read.c
@@ -0,0 +1,54 @@
+#include "roku_redis_connect.h"
+#include "roku_redis_return_codes.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+
+return_code roku_redis_read(char **result)
+{
+  int poll_status = poll(&conn, 1, 10000);
+
+  if(poll_status == 0)
+  {
+    return SOCKET_TIMEOUT;
+  }
+
+  int total_bytes = 0;
+  int bytes;
+  int heap_buffer_size = 10;
+  char *heap_buffer = malloc(heap_buffer_size);
+
+  int tmp_buffer_size = 5;
+  char tmp_buffer[tmp_buffer_size];
+
+  while(conn.revents & POLLIN)
+  {
+    bytes = read(conn.fd, tmp_buffer, tmp_buffer_size);
+    tmp_buffer[bytes] = 0;
+    total_bytes += bytes;
+
+    if(bytes == 0)
+      break;
+
+    if(total_bytes > heap_buffer_size)
+    {
+      heap_buffer_size = heap_buffer_size * 10;
+      char *new_heap_ptr = realloc(heap_buffer, heap_buffer_size);
+
+      if(new_heap_ptr)
+        heap_buffer = new_heap_ptr;
+      else
+      {
+        return SOCKET_MEMORY_FAILED;
+      }
+     }
+
+    memcpy(heap_buffer + total_bytes - bytes, tmp_buffer, bytes);
+  }
+
+  heap_buffer[total_bytes] = 0;
+  *result = heap_buffer;
+  return SUCCESS;
+}
---
+++ roku_redis_read.h
@@ -0,0 +1,8 @@
+#ifndef _ROKU_REDIS_READ_
+#define _ROKU_REDIS_READ_
+
+#include "roku_redis_return_codes.h"
+
+return_code roku_redis_read(char **result);
+
+#endif
