1969cf43a086043127603b452c252bb49b6958c9
roku6185 <robin@alike.se>
2016-04-27 21:37:24 +0200

    Added function to send an redis object to the server

--- roku_redis_write.c
+++ roku_redis_write.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
+#include <string.h>
 #include "roku_redis_connect.h"
 #include "roku_redis_return_codes.h"
-
+#include "roku_redis_object.h"
 
 return_code roku_redis_write(const char *data, int length)
 {
@@ -14,4 +15,10 @@ return_code roku_redis_write(const char *data, int length)
   }
   
   return SOCKET_WRITE_FAILED;
+}
+
+return_code roku_redis_send_command(redis_object *command)
+{
+  const char *tmp = redis_serialize_object(command);
+  return roku_redis_write(tmp, strlen(tmp));
 }
--- roku_redis_write.h
+++ roku_redis_write.h
@@ -2,7 +2,9 @@
 #define _ROKU_REDIS_WRITE_
 
 #include "roku_redis_return_codes.h"
+#include "roku_redis_object.h"
 
+return_code roku_redis_send_command(redis_object *command);
 return_code roku_redis_write(const char *data, int length);
 
 #endif
