521787a35dea71d7b33a933715c5307514e2b760
roku6185 <robin@alike.se>
2016-04-27 21:38:03 +0200

    Added actual test program

---
+++ apps/main.c
@@ -0,0 +1,43 @@
+#include "roku_redis_connect.h"
+#include "roku_redis_write.h"
+#include "roku_redis_read.h"
+#include "roku_redis_object.h"
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  return_code status;
+
+  if((status = roku_redis_connect("127.0.0.1", "6379")) == SUCCESS)
+    printf("Connected.\n");
+  else
+  {
+    printf("Connection failed.\n");
+    return 1;
+  }
+  
+  redis_object *param1 = redis_create_bulk_string("GET", 3);
+  redis_object *param2 = redis_create_bulk_string("mykey", 5);
+  redis_object *cmd = redis_create_array();
+  redis_array_push_back(cmd, param1);
+  redis_array_push_back(cmd, param2);
+  
+  if((status = roku_redis_send_command(cmd)) == SUCCESS)
+    printf("Command was sent successfully.\n");
+  else
+  {
+    printf("Sending command failed.\n");
+    return 1;
+  }
+  
+  char *buffer = NULL;
+  if((status = roku_redis_read(&buffer)) == SUCCESS)
+  {
+    redis_object *response = redis_deserialize_object(buffer);
+    printf("Received response:\n");
+    redis_pretty_print_object(response);
+  }
+  
+  return 0;
+}
