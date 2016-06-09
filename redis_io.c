4b60040ba308231c607087c05c6c63b0893c1f11
roku6185 <robin@alike.se>
2016-06-09 21:46:27 +0200

    Added better error handling for read/write

--- redis_io.c
+++ redis_io.c
@@ -1,27 +1,143 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include "redis_connect.h"
 #include "redis_return_codes.h"
 #include "redis_object.h"
 
 return_code redis_write(const char *data, int length)
 {
-  int bytes = 0;
+  if(conn.fd <= 0)
+    return SOCKET_WRITE_FAILED;
 
-  if(conn.fd)
+  while(length > 0)
   {
-    bytes = write(conn.fd, data, length);
-    return SUCCESS;
+    int bytes = write(conn.fd, data, length);
+    
+    if(bytes == 0)
+    {
+      return SOCKET_DISCONNECT;
+    }
+    else if(bytes == -1)
+    {
+      if(errno == EAGAIN)
+      {
+        // Blocked. Try again.
+        continue;
+      }
+      else if(errno == EINTR)
+      {
+        // Interrupted by a signal. Try again.
+        continue;
+      }
+      else
+      {
+        return SOCKET_WRITE_FAILED;
+      }
+    }
+    
+    length -= bytes;
+    data += bytes;
   }
   
-  return SOCKET_WRITE_FAILED;
+  return SUCCESS;
+}
+
+return_code redis_read(char **result)
+{
+  conn.events = POLLIN;
+  conn.revents = 0;
+  int poll_status = poll(&conn, 1, 10000);
+  
+  if(poll_status == 0)
+  {
+    return SOCKET_TIMEOUT;
+  }
+
+  int total_bytes = 0;
+  int bytes;
+  int heap_buffer_size = 1024;
+  char *heap_buffer = malloc(heap_buffer_size);
+
+  int tmp_buffer_size = 256;
+  char tmp_buffer[tmp_buffer_size];
+
+  while(conn.revents & POLLIN)
+  {
+    bytes = read(conn.fd, tmp_buffer, tmp_buffer_size);
+    
+    if(bytes == 0)
+    {
+      break;
+    }
+    else if(bytes == -1)
+    {
+      if(errno == EAGAIN)
+      {
+        // Blocked. Try again.
+        continue;
+      }
+      else if(errno == EINTR)
+      {
+        // Interrupted by signal. Try again.
+        continue;
+      }
+      else
+      {
+        return SOCKET_READ_FAILED;
+      }
+    }
+      
+    tmp_buffer[bytes] = 0;
+    total_bytes += bytes;
+
+    if(total_bytes > heap_buffer_size)
+    {
+      heap_buffer_size = heap_buffer_size * 10;
+      char *new_heap_ptr = realloc(heap_buffer, heap_buffer_size);
+
+      if(new_heap_ptr)
+        heap_buffer = new_heap_ptr;
+      else
+      {
+        return SOCKET_MEMORY_FAILED;
+      }
+     }
+
+    memcpy(heap_buffer + total_bytes - bytes, tmp_buffer, bytes);
+    conn.events = POLLIN;
+    conn.revents = 0;
+  }
+  
+  heap_buffer[total_bytes] = 0;
+  *result = heap_buffer;
+  return SUCCESS;
 }
 
 return_code redis_send_command(redis_object *command)
 {
   redis_serialization cmd_serialized = redis_serialize_object(command);
-  return_code status = redis_write(cmd_serialized.value, cmd_serialized.length);
+  // Don't send trailing null byte
+  return_code status = redis_write(cmd_serialized.value, cmd_serialized.length - 1);
   free((char *)cmd_serialized.value);
   return status;
+}
+
+
+redis_object *redis_send_and_receive_command(redis_object *command, return_code *status)
+{
+  char *buffer = NULL;
+  
+  if((*status = redis_send_command(command)) != SUCCESS)
+    return NULL;
+    
+  if((*status = redis_read(&buffer)) != SUCCESS)
+    return NULL;
+    
+  redis_object *object = redis_deserialize_object(buffer);
+  free(buffer);
+  return object;
 }
--- redis_io.h
+++ redis_io.h
@@ -4,7 +4,9 @@
 #include "redis_return_codes.h"
 #include "redis_object.h"
 
+redis_object *redis_send_and_receive_command(redis_object *command, return_code *status);
 return_code redis_send_command(redis_object *command);
+return_code redis_read(char **result);
 return_code redis_write(const char *data, int length);
 
 #endif
